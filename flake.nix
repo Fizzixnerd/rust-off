e522f0ae407cb221c4995f625a515b9b85a1e289
Matt Walker <matt.g.d.walker@gmail.com>
2023-04-01 08:34:48 -0400

    try again

--- flake.nix
+++ flake.nix
@@ -1,31 +1,44 @@
-# This file is pretty general, and you can adapt it in your project replacing
-# only `name` and `description` below.
-
 {
-  description = "My awesome Rust project";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
+  inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
-    nci.url = "github:yusdacra/nix-cargo-integration";
-    nci.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = { self, nixpkgs, nci, ... }:
-    nci.lib.makeOutputs {
-      # Documentation and examples:
-      # https://github.com/yusdacra/rust-nix-templater/blob/master/template/flake.nix
-      root = ./.;
-      overrides = {
-        shell = common: prev: {
-          packages = prev.packages ++ [
-            common.pkgs.rust-analyzer
-            common.pkgs.cargo-watch
-          ];
+  outputs = inputs @ {
+    parts,
+    nci,
+    ...
+  }:
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [nci.flakeModule];
+      perSystem = {
+        pkgs,
+        config,
+        ...
+      }: let
+        # TODO: change this to your crate's name
+        crateName = "frust-off";
+        # shorthand for accessing this crate's outputs
+        # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
+        crateOutputs = config.nci.outputs.${crateName};
+      in {
+        # declare projects
+        # relPath is the relative path of a project to the flake root
+        # TODO: change this to your crate's path
+        nci.projects.${crateName}.relPath = "./";
+        # configure crates
+        nci.crates.${crateName} = {
+          # export crate (packages and devshell) in flake outputs
+          # alternatively you can access the outputs and export them yourself (see below)
+          export = true;
+          # look at documentation for more options
         };
+        # export the crate devshell as the default devshell
+        devShells.default = crateOutputs.devShell;
+        # export the release package of the crate as default package
+        packages.default = crateOutputs.packages.release;
       };
     };
 }
